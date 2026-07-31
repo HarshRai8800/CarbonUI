d13dcbc3f2bce7c81073b27da1d9ec88feb0426b
Harsh Rai <153977193+HarshRai8800@users.noreply.github.com>
2026-08-01 00:22:09 +0530

    Update isAuth.js

--- carbonui-server/middlewares/isAuth.js
+++ carbonui-server/middlewares/isAuth.js
@@ -15,6 +15,7 @@ const isAuth = async(req,res,next)=>{
         req.userId = verifyToken.userId;
         next();
     } catch (error) {
+        console.log(error)
         return res.status(500).json({message:"isAuth middleware error, "+error});
     }
 }
