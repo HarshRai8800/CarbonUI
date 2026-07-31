61ea7c7f8266a1614075b4f5e5a8efa53cf205f4
Harsh Rai <153977193+HarshRai8800@users.noreply.github.com>
2026-08-01 00:26:34 +0530

    Update auth.controllers.js

--- carbonui-server/controllers/auth.controllers.js
+++ carbonui-server/controllers/auth.controllers.js
@@ -14,10 +14,12 @@ export const googleAuth = async(req,res)=>{
         
         res.cookie("token",token,{
             httpOnly : false,
-            secue:true,
+            secure:true,
             sameSite:"none",
             maxAge:7*24*60*60*1000
         })
+
+        cosole.log( "user:  "+user)
 
         return res.status(200).json(user);
 
@@ -30,7 +32,7 @@ export const logOut = async(req,res)=>{
     try{
         await res.clearCookie("token",{
             httpOnly:false,
-            secue:true,
+            secure:true,
             sameSite:"none"
         })
         return res.status(200).json({message:"LogOut Successfully"});
