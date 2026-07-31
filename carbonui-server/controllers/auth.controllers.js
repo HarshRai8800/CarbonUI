04b873044b7fecf62d2adbde5ddf05c236da2da2
Harsh Rai <153977193+HarshRai8800@users.noreply.github.com>
2026-08-01 01:35:39 +0530

    Refactor googleAuth and logOut functions for clarity

--- carbonui-server/controllers/auth.controllers.js
+++ carbonui-server/controllers/auth.controllers.js
@@ -1,48 +1,58 @@
 import User from "../models/user.model.js";
-import {genToken} from "../configs/token.js";
+import { genToken } from "../configs/token.js";
 
-export const googleAuth = async(req,res)=>{
-    try{
-        const {name,email} = req.body;
-        let user = await User.findOne({email});
-        if(!user){
+export const googleAuth = async (req, res) => {
+    try {
+        const { name, email } = req.body;
+
+        let user = await User.findOne({ email });
+
+        if (!user) {
             user = await User.create({
-                name,email
-            })
+                name,
+                email
+            });
         }
-        let token = await genToken(user._id);
-        
-        res.cookie("token",token,{
-            httpOnly : false,
-            secure:true,
-            sameSite:"none",
-            maxAge:7*24*60*60*1000
-        })
 
-        console.log( "user:  "+user)
+        const token = await genToken(user._id);
+
+        res.cookie("token", token, {
+            httpOnly: false,
+            secure: true,
+            sameSite: "none",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
+        console.log("User:", user);
 
         return res.status(200).json(user);
 
-    }catch(err){
-        catch (err) {
+    } catch (err) {
         console.error("Google Auth Error:", err);
 
         return res.status(500).json({
-        message: err.message
-    });
-} 
+            message: err.message
+        });
     }
-}
+};
 
-export const logOut = async(req,res)=>{
-    try{
-        await res.clearCookie("token",{
-            httpOnly:false,
-            secure:true,
-            sameSite:"none"
-        })
-        return res.status(200).json({message:"LogOut Successfully"});
-    }catch(err){
-        return res.status(500).json({message:`Failed to log out ${err}`});  
+export const logOut = async (req, res) => {
+    try {
+        res.clearCookie("token", {
+            httpOnly: false,
+            secure: true,
+            sameSite: "none"
+        });
+
+        return res.status(200).json({
+            message: "LogOut Successfully"
+        });
+
+    } catch (err) {
+        console.error("Logout Error:", err);
+
+        return res.status(500).json({
+            message: err.message
+        });
     }
-}
+};
