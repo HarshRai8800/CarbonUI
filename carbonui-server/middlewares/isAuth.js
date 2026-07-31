a28dca579798cec12ca466e4848dbfac44f7796f
Harsh Rai <153977193+HarshRai8800@users.noreply.github.com>
2026-07-31 23:49:19 +0530

    testing1

--- carbonui-server/middlewares/isAuth.js
+++ carbonui-server/middlewares/isAuth.js
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken";
 const isAuth = async(req,res,next)=>{
     try {
         let {token} = req.cookies;
+        console.log(token);
         if(!token){
             return res.status(400).json({message:"User does not have token"});
         }
@@ -10,6 +11,7 @@ const isAuth = async(req,res,next)=>{
             if(!verifyToken){
                 return res.status(400).json({message:"User does not have valid token"});
             }
+        console.log(verifyToken.userId);
         req.userId = verifyToken.userId;
         next();
     } catch (error) {
